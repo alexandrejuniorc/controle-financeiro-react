40b21f2ed5f9a6f3635608ec7452f4fad9ea88bd
Alexandre Junior <alexandrejuniordev@gmail.com>
2022-08-26 15:24:24 -0300

    inicio da lógica para adicionar transação e mostrar na lista de extrato

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,26 +5,54 @@ import { TransationContainer } from '../../components/Transation/styles';
 import { ExtractContainer } from '../../components/Extract/styles';
 
 export const Home = () => {
+  // transaction data
   const [merchandiseName, setMerchandiseName] = useState('');
   const [commodityName, setCommodityName] = useState('');
   const [valueInput, setValueInput] = useState('');
 
-  const [extractList, setExtractList] = useState([]);
-
-  const drawTable = () => {
-    let total = 0;
-  };
+  const [extractList, setExtractList] = useState<any>([]);
+  const [itemsList, setItemsList] = useState<any>([]);
 
   const dataClean = () => {
     const confirmUser = confirm('Deseja remover todas as transações?');
     if (confirmUser) {
       localStorage.clear();
       setExtractList([]);
-      drawTable();
     }
   };
 
-  const sentTransaction = () => {};
+  const drawTable = () => {
+    let total = 0;
+    let maskValue;
+
+    for (const product in extractList) {
+      if (extractList[product].merchandiseName === 'compra') {
+        maskValue = extractList[product];
+        total -= Number(extractList[product].valueInput);
+      } else {
+        total += Number(extractList[product].valueInput);
+      }
+
+      console.log(extractList);
+    }
+  };
+
+  const sentTransaction = (e: any) => {
+    e.preventDefault();
+
+    extractList.push({
+      merchandiseName,
+      commodityName,
+      valueInput: valueInput.replaceAll('.', '').replaceAll(',', '.'),
+    });
+
+    const stringList = JSON.stringify(extractList);
+    localStorage.setItem('lista', stringList);
+
+    drawTable();
+  };
+
+  drawTable();
 
   return (
     <HomeContainer>
@@ -71,11 +99,15 @@ export const Home = () => {
         </div>
 
         <div>
-          <div className="merchandise">
-            <span>+</span>
-            <p>Lorem ipsum dolor sit amet consectetur</p>
-            <span>R$ 12.999,99</span>
-          </div>
+          {extractList!.map((product: any) => {
+            return (
+              <div key={product.merchandiseName} className="merchandise">
+                <span>{product.commodityName}</span>
+                <p>{commodityName}</p>
+                <span>{valueInput}</span>
+              </div>
+            );
+          })}
 
           <div className="total">
             <span>Total</span>
@@ -89,6 +121,3 @@ export const Home = () => {
     </HomeContainer>
   );
 };
-function useForm(arg0: string) {
-  throw new Error('Function not implemented.');
-}
